a8d825a506cef271ff3703c8e3118d79bef78353
quocleeVN <lebaoquoc275@gmail.com>
2026-03-26 16:57:14 +0700

    baoquoc updateeee

--- routes/web.php
+++ routes/web.php
@@ -62,44 +62,44 @@ Route::get('/vananh2', function () {
 });
 
 
-Route::get('/genres', function(){
+Route::get('/genres', function () {
     $genres = DB::table('genre')->get();
     return view('viewmovie.genres', compact('genres'));
 });
 Route::get('/topvote', function () {
     $movies = DB::table('movie')
-            ->orderBy('vote_average', 'desc')
-            ->limit(10)
-            ->get();
+        ->orderBy('vote_average', 'desc')
+        ->limit(10)
+        ->get();
     return view('viewmovie.topvote', compact('movies'));
 });
 Route::get('/topbudget', function () {
     $movies = DB::table('movie')
-            ->orderBy('budget', 'desc')
-            ->limit(10)
-            ->get();
+        ->orderBy('budget', 'desc')
+        ->limit(10)
+        ->get();
     return view('viewmovie.topbudget', compact('movies'));
 });
 Route::get('/runtime', function () {
     $movies = DB::table('movie')
-            ->where('runtime', '>', 120)
-            ->limit(10)
-            ->get();
+        ->where('runtime', '>', 120)
+        ->limit(10)
+        ->get();
     return view('viewmovie.runtime', compact('movies'));
 });
 
 Route::get('/country', function () {
     $countries = DB::table('movie')
-            ->select('country_name')
-            ->distinct()
-            ->get();
+        ->select('country_name')
+        ->distinct()
+        ->get();
     return view('viewmovie.country', compact('countries'));
 });
 Route::post('/country_result', function (Illuminate\Http\Request $request) {
     $movies = DB::table('movie')
-            ->where('country_name', $request->country)
-            ->select('movie_name', 'release_date', 'runtime')
-            ->get();
+        ->where('country_name', $request->country)
+        ->select('movie_name', 'release_date', 'runtime')
+        ->get();
     return view('viewmovie.country_result', compact('movies'));
 });
 
@@ -109,16 +109,16 @@ Route::get('/genre', function () {
 });
 Route::post('/genre_result', function (Illuminate\Http\Request $request) {
     $movies = DB::table('movie')
-            ->where('movie_name', 'like', '%') // fallback nếu chưa có bảng trung gian
-            ->get();
+        ->where('movie_name', 'like', '%') // fallback nếu chưa có bảng trung gian
+        ->get();
     return view('viewmovie.genre_result', compact('movies'));
 });
 
 Route::get('/hotmovie', function () {
     $movies = DB::table('movie')
-            ->where('vote_average', '>', 8)
-            ->where('vote_count', '>', 10000)
-            ->get();
+        ->where('vote_average', '>', 8)
+        ->where('vote_count', '>', 10000)
+        ->get();
     return view('viewmovie.hotmovie', compact('movies'));
 });
 
@@ -147,4 +147,5 @@ Route::get('7.7', [Controller_movie::class, 'vote_movie']);
 
 Route::get('/sach', [BookController::class, 'book']);
 Route::get('/sach/theloai/{id}', [BookController::class, 'theloai']);
-Route::get('/sach/chitiet/{id}', [BookController::class, 'chitiet']);
+
+Route::get('/sach/chitiet/{id}', [ChiTietSachController::class, 'show'])->name('book.detail');
